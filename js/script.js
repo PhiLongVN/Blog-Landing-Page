e523b788367ce09fc555191f74eae54bd4f8d4db
Phi Long <91886384+PhiLongVN@users.noreply.github.com>
2021-12-29 16:05:50 +0700

    upload3

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-const cclose = document.querySelector(".close")
-const box = document.querySelector(".box-head")
-const menu1 = document.querySelector(".menu1")
-const menu2 = document.querySelector(".menu2")
-const menu3 = document.querySelector(".menu3")
+const cclose = document.querySelector(".close");
+const box = document.querySelector(".box-head");
+const menu1 = document.querySelector(".menu1");
+const menu2 = document.querySelector(".menu2");
+const menu3 = document.querySelector(".menu3");
+const hamber = document.querySelector(".close img:nth-of-type(1)");
 
+cclose.onclick = () => {
+  box.classList.toggle("active");
+  cclose.classList.toggle("active");
+};
 
-cclose.onclick = () =>{
-    box.classList.toggle("active")
-}
-
-menu1.onclick = () =>{
-    menu1.classList.toggle("active")
-}
-menu2.onclick = () =>{
-    menu2.classList.toggle("active")
-}
-menu3.onclick = () =>{
-    menu3.classList.toggle("active")
-}
-
+menu1.onclick = () => {
+  menu1.classList.toggle("active");
+};
+menu2.onclick = () => {
+  menu2.classList.toggle("active");
+};
+menu3.onclick = () => {
+  menu3.classList.toggle("active");
+};
